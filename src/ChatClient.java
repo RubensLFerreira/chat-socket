c3024e3a07369d409a58d6a7148c0872e8fcc536
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2022-02-28 21:41:38 -0300

    😀 Subindo projeto chat-socket

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -2,20 +2,22 @@ import java.io.IOException;
 import java.net.Socket;
 
 public class ChatClient {
-  private static  final String SERVER_ADDRESS = "127.0.0.1"; // adicionando o endereço do servidor
+  private static  final String SERVER_ADDRESS = "192.168.0.12"; // adicionando o endereço do servidor
   private Socket clienteSocket; // criando o canal lado cliente
 
   public void start() throws IOException {
-    clienteSocket = new Socket("127.0.0.1", ChatServer.PORT); // interligando com o server
+    clienteSocket = new Socket(SERVER_ADDRESS, ChatServer.PORT); // interligando com o server
+    System.out.println("Cliente conectado ao servidor em " +  SERVER_ADDRESS + ": " + ChatServer.PORT);
   }
   public static void main(String[] args) {
     try {
       ChatClient cliente = new ChatClient();
       cliente.start();
     } catch (IOException e) {
-      //System.out.println("Erro ao iniciar cliente: " + e.getMessage());
+      System.out.println("Erro ao iniciar cliente: " + e.getMessage());
       e.printStackTrace();
 
     }
+    System.out.println("Cliente finalizado!");
   }
 }
