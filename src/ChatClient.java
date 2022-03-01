4ba7a6635ae1fe8269d00aa2093108284c3b6450
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2022-03-01 16:27:57 -0300

    🚀 Finalizando projeto chat-socket

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -4,7 +4,7 @@ import java.net.Socket;
 import java.sql.SQLClientInfoException;
 import java.util.Scanner;
 
-public class ChatClient {
+public class ChatClient implements Runnable{
   private static  final String SERVER_ADDRESS = "192.168.0.12"; // adicionando o endereço do servidor
   private ClientSocket clienteSocket; // criando o canal lado cliente
   private Scanner scanner;
@@ -15,16 +15,28 @@ public class ChatClient {
   }
 
   public void start() throws IOException {
-    clienteSocket = new ClientSocket(new Socket(SERVER_ADDRESS, ChatServer.PORT)); // interligando com o server
-    //clienteSocket.getOutputStream(); // saída de dados (p/ enviar dados)
-    System.out.println("Cliente conectado ao servidor em " +  SERVER_ADDRESS + ": " + ChatServer.PORT);
-    messageLoop();
+    try {
+      clienteSocket = new ClientSocket(new Socket(SERVER_ADDRESS, ChatServer.PORT)); // interligando com o server
+      //clienteSocket.getOutputStream(); // saída de dados (p/ enviar dados)
+      System.out.println("Cliente conectado ao servidor em " +  SERVER_ADDRESS + ": " + ChatServer.PORT);
+      new Thread(this).start();
+      messageLoop();
+    } finally {
+      clienteSocket.close();
+    }
+  }
+  @Override
+  public void run() {
+    String msg;
+    while((msg = clienteSocket.getMessage()) != null) {
+      System.out.printf("Mensagem recebida do servidor: %s\n", msg);
+    }
   }
 
   private void messageLoop() throws IOException {
     String msg;
     do{
-      System.out.print("Digite uma mensagem (ou 'tchau' para finalizar): ");
+      System.out.println("Digite uma mensagem (ou 'tchau' para finalizar): ");
       msg = scanner.nextLine(); // receber uma string digitada pelo cliente
       clienteSocket.sendMsg(msg);
     }while(!msg.equalsIgnoreCase("tchau"));
--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,12 +1,13 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
-import java.net.Socket;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 public class ChatServer {
   public static final int PORT = 4000; // declarando a porta
   private ServerSocket serverSocket; // declarando o socket
+  private final List<ClientSocket> clients = new LinkedList<>();
 
   public void start() throws IOException {
     serverSocket = new ServerSocket(PORT); // iniciando o servidor
@@ -17,10 +18,11 @@ public class ChatServer {
   private void clientConnectionLoop() throws IOException {
     while(true){
       ClientSocket clienteSocket = new ClientSocket(serverSocket.accept()); //(cliente local) vai aguardar um cliente conectar
+      clients.add(clienteSocket);
       new Thread(() -> clientMessageLoop(clienteSocket)).start();
     }
   }
-  public void clientMessageLoop(ClientSocket clientSocket) { // receber msgs em loop
+  private void clientMessageLoop(ClientSocket clientSocket) { // receber msgs em loop
     String msg;
     try {
       while((msg = clientSocket.getMessage()) != null) {
@@ -30,11 +32,25 @@ public class ChatServer {
         System.out.printf("Mensagem recebida do cliente %s: %s\n",
                 clientSocket.getRemoteSocketAddress(),
                 msg);
+        sendMsgToAll(clientSocket, msg);
       }
     } finally {
       clientSocket.close();
     }
   }
+
+  private void sendMsgToAll(ClientSocket sender, String msg) {
+    Iterator<ClientSocket> iterator = clients.iterator();
+    while (iterator.hasNext()) {
+      ClientSocket clientSocket = iterator.next();
+      if (!sender.equals(clientSocket)) {
+        if (!clientSocket.sendMsg("cliente " + sender.getRemoteSocketAddress() + ": " + msg)) {
+          iterator.remove();
+        }
+      }
+    }
+  }
+
   public static void main(String[] args) {
     try {
       ChatServer server = new ChatServer();
