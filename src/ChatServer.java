c308216fdfd179cf21a51687d69128a5c3e430c6
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2022-02-28 21:41:38 -0300

    😀 Subindo projeto chat-socket

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,20 +1,29 @@
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class ChatServer {
   public static final int PORT = 4000; // declarando a porta
   private ServerSocket serverSocket; // declarando o socket
 
   public void start() throws IOException {
-    serverSocket = new ServerSocket(4000); // iniciando o servidor
+    serverSocket = new ServerSocket(PORT); // iniciando o servidor
+    System.out.println("Servidor iniciado na porta: " + PORT);
+    clientConnectionLoop();
   }
 
+  private void clientConnectionLoop() throws IOException {
+    while(true){
+      Socket clienteSocket = serverSocket.accept(); //(cliente local) vai aguardar um cliente conectar
+      System.out.println("Cliente: " + clienteSocket.getRemoteSocketAddress() + " conectou!"); // buscando o cliente pelo endereço
+    }
+  }
   public static void main(String[] args) {
     try {
       ChatServer server = new ChatServer();
       server.start();
     } catch (IOException e) {
-      e.printStackTrace();
+      System.out.println("Erro ao iniciar o servidor " + e.getMessage());
     }
   }
 }
