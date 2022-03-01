f0027355fb70d1d529daa7df95f67c9776eed7b0
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2022-03-01 13:15:01 -0300

    🚀 Subindo projeto chat-socket

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -14,8 +16,23 @@ public class ChatServer {
 
   private void clientConnectionLoop() throws IOException {
     while(true){
-      Socket clienteSocket = serverSocket.accept(); //(cliente local) vai aguardar um cliente conectar
-      System.out.println("Cliente: " + clienteSocket.getRemoteSocketAddress() + " conectou!"); // buscando o cliente pelo endereço
+      ClientSocket clienteSocket = new ClientSocket(serverSocket.accept()); //(cliente local) vai aguardar um cliente conectar
+      new Thread(() -> clientMessageLoop(clienteSocket)).start();
+    }
+  }
+  public void clientMessageLoop(ClientSocket clientSocket) { // receber msgs em loop
+    String msg;
+    try {
+      while((msg = clientSocket.getMessage()) != null) {
+        if ("tchau".equalsIgnoreCase(msg))
+          return;
+
+        System.out.printf("Mensagem recebida do cliente %s: %s\n",
+                clientSocket.getRemoteSocketAddress(),
+                msg);
+      }
+    } finally {
+      clientSocket.close();
     }
   }
   public static void main(String[] args) {
